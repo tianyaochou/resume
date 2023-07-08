7aee68bdef1c94cdfad26775dd557254176aa032
tianyaochou <tianyaochou@fastmail.com>
2023-07-09 01:23:22 +0200

    Fix font

--- flake.nix
+++ flake.nix
@@ -5,14 +5,17 @@
   outputs = { self, nixpkgs, flake-utils, }: flake-utils.lib.eachDefaultSystem (system:
   let pkgs = nixpkgs.legacyPackages.${system};
       stdenv = pkgs.stdenv;
+      fonts = [ pkgs.iosevka ];
+      fontPaths = builtins.concatStringsSep ":" fonts;
   in 
   {
     packages = rec {
       resume = stdenv.mkDerivation {
         name = "resume";
-        buildInputs = [ pkgs.typst pkgs.iosevka ];
+        buildInputs = [ pkgs.typst ];
         src = ./.;
         buildPhase = ''
+          export TYPST_FONT_PATHS=${fontPaths}
           typst compile main.typ resume.pdf
         '';
         installPhase = ''
